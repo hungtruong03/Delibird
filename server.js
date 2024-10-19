1487a23cf56fab09ed7791f7ed4cead94fa97a33
21120079 <hungtruonghl11@gmail.com>
2024-10-19 14:53:32 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
