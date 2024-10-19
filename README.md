a0dcad069b1be3d247dee2b509072be409a93a31
21120079 <hungtruonghl11@gmail.com>
2024-10-19 15:17:29 +0700

    Add basic files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Delibird
+
+# Submodule - Tạo OTP và gửi email
+
+## Giới thiệu
+
+Đây là submodule của SkillSharp.
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const Routers = require('./routers/main_router.js');
+
+app.use(express.json());
+
+app.use('/otp', Routers);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
